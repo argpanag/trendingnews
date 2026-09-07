bced4eb1be3ac84822f4439fd53f37d618ef8c28
test <test@test.com>
2026-09-07 18:52:52 +0300

    ddd

--- api/run.php
+++ api/run.php
@@ -77,7 +77,7 @@ function pushToGithub(): array {
     $commands = [
         'git add -A',
         'git diff --cached --quiet || git commit -m "chore: manual update ' . date('Y-m-d\TH:i:s') . '"',
-        'git push origin HEAD:master',
+        'git push origin HEAD:main',
     ];
 
     $output = [];
